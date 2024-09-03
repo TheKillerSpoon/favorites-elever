8153969025b41e9453f23cf91e569e78422a9e03
TheKillerSpoon <75366115+TheKillerSpoon@users.noreply.github.com>
2024-09-03 09:27:27 +0200

    lkmnjm

---
+++ js/favorites.js
@@ -0,0 +1,7 @@
+export function favorites() {
+  const favoriteListContainer = document.querySelector(".favorite-container");
+
+  localStorage.setItem("favList", "test");
+
+  
+}
--- js/index.js
+++ js/index.js
@@ -1,14 +1,13 @@
-import { productsList } from "./products_list.js"
-import { productsPage } from "./products_page.js"
+import { productsList } from "./products_list.js";
+import { productsPage } from "./products_page.js";
+import { favorites } from "./favorites.js";
 
-const app = {}
+const app = {};
 
 app.init = async () => {
+  productsList();
+  productsPage();
+  favorites();
+};
 
-    productsList()
-    productsPage()
-
-}
-
-
-app.init()
+app.init();
